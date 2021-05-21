2b7607ba482218221ecaecec1eb79b354702d31c
higemoja <higemoja@gmail.com>
2021-05-21 14:31:56 +0900

    iOS buildのためcrashlytics versionを下げる

--- minden/lib/main.dart
+++ minden/lib/main.dart
@@ -11,12 +11,14 @@ void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await di.init();
 
+  Crashlytics.instance.enableInDevMode = true;
+  FlutterError.onError = Crashlytics.instance.recordFlutterError;
+
   if (kReleaseMode) {
     runZonedGuarded(() async {
       runApp(Application());
-    }, (e, s) async => await FirebaseCrashlytics.instance.recordError(e, s));
+    }, (e, s) async => await Crashlytics.instance.recordError(e, s));
   } else {
-    await FirebaseCrashlytics.instance.setCrashlyticsCollectionEnabled(false);
     runApp(Application());
   }
 }
