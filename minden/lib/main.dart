1d2b1a4a5314449fd9ae378589b6ea7966c6c47b
higemoja <higemoja@gmail.com>
2021-05-25 15:04:38 +0900

    staging環境設定を追加

--- minden/lib/main.dart
+++ minden/lib/main.dart
@@ -6,18 +6,18 @@ import 'package:firebase_crashlytics/firebase_crashlytics.dart';
 import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:minden/application.dart';
-
+import 'package:minden/core/env/config.dart';
 import 'package:minden/injection_container.dart' as di;
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
+  const env = String.fromEnvironment("DEFINE_BUILD_ENV");
+  Config.setEnvironment(env);
   await di.init();
 
   Crashlytics.instance.enableInDevMode = true;
   FlutterError.onError = Crashlytics.instance.recordFlutterError;
 
-  const env = String.fromEnvironment("DEFINE_BUILD_ENV");
-  print("${env}");
   if (kReleaseMode) {
     runZonedGuarded(() async {
       runApp(Application());
