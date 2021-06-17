0e04679343e7bc99e12723829fba3f209e603ec5
machida-yosuke <machida-yosuke@kayac.com>
2021-06-17 10:50:59 +0900

    FB

--- minden/lib/features/startup/presentation/pages/tutorial_page.dart
+++ minden/lib/features/startup/presentation/pages/tutorial_page.dart
@@ -1,5 +1,8 @@
 import 'package:carousel_slider/carousel_slider.dart';
 import 'package:flutter/material.dart';
+import 'package:minden/core/util/string_util.dart';
+import 'package:minden/utile.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 
 class Tutorial {
   late final String title;
@@ -14,36 +17,34 @@ class Tutorial {
 }
 
 class TutorialPage extends StatefulWidget {
-  TutorialPage({Key? key}) : super(key: key);
-
   @override
   _TutorialPageState createState() => _TutorialPageState();
 }
 
 class _TutorialPageState extends State<TutorialPage> {
   final CarouselController _controller = CarouselController();
-  List<Tutorial> tutorialData = [
-    Tutorial(
-        imagePath: 'tutorial-1.png',
-        title: '電気の生産者の顔が\n見える・つながる',
-        description:
-            '何でも薄暗いじめじめした所でニャーニャー泣いていた\n事だけは記憶している。\n吾輩はここで始めて人間というものを見た。'),
-    Tutorial(
-        imagePath: 'tutorial-2.png',
-        title: '電気の使用量や\n料金の行き先が見える',
-        description:
-            '何でも薄暗いじめじめした所でニャーニャー泣いていた\n事だけは記憶している。\n吾輩はここで始めて人間というものを見た。'),
-    Tutorial(
-        imagePath: 'tutorial-3.png',
-        title: 'あの有名企業も使っている',
-        description:
-            '何でも薄暗いじめじめした所でニャーニャー泣いていた\n事だけは記憶している。\n吾輩はここで始めて人間というものを見た。'),
-  ];
-
   int _currentIndex = 0;
 
   @override
   Widget build(BuildContext context) {
+    List<Tutorial> _tutorialData = [
+      Tutorial(
+        imagePath: 'tutorial-1.png',
+        title: i18nTranslate(context, 'tutorial_step_1_title'),
+        description: i18nTranslate(context, 'tutorial_step_1_description'),
+      ),
+      Tutorial(
+        imagePath: 'tutorial-2.png',
+        title: i18nTranslate(context, 'tutorial_step_2_title'),
+        description: i18nTranslate(context, 'tutorial_step_2_description'),
+      ),
+      Tutorial(
+        imagePath: 'tutorial-3.png',
+        title: i18nTranslate(context, 'tutorial_step_3_title'),
+        description: i18nTranslate(context, 'tutorial_step_3_description'),
+      ),
+    ];
+
     return Scaffold(
       body: SizedBox.expand(
         child: Container(
@@ -64,7 +65,7 @@ class _TutorialPageState extends State<TutorialPage> {
                       });
                     }),
                 carouselController: _controller,
-                items: tutorialData.map((Tutorial data) {
+                items: _tutorialData.map((Tutorial data) {
                   return Builder(
                     builder: (BuildContext context) {
                       return Slide(data: data);
@@ -82,28 +83,30 @@ class _TutorialPageState extends State<TutorialPage> {
                       width: 47,
                       height: 17,
                       child: GestureDetector(
-                        onTap: () {
+                        onTap: () async {
                           // ここで別ページに飛ばす
+                          await setHasTutorial();
                           print('skip');
                         },
                         child: Opacity(
-                          opacity: _currentIndex != tutorialData.length - 1
+                          opacity: _currentIndex != _tutorialData.length - 1
                               ? 1.0
                               : 0.0,
                           child: Text(
-                            'Skip',
+                            i18nTranslate(context, "skip"),
                             style: TextStyle(
                               fontFamily: 'NotoSansJP',
                               fontWeight: FontWeight.w400,
                               fontSize: 14,
                               color: Color(0xFF000000),
+                              letterSpacing: calcLetterSpacing(letter: 4),
                             ),
                           ),
                         ),
                       ),
                     ),
                     Row(
-                      children: tutorialData.asMap().entries.map((entry) {
+                      children: _tutorialData.asMap().entries.map((entry) {
                         int index = entry.key;
                         return Container(
                           width: 7,
@@ -121,7 +124,7 @@ class _TutorialPageState extends State<TutorialPage> {
                     Container(
                       width: 47,
                       height: 17,
-                      child: _currentIndex != tutorialData.length - 1
+                      child: _currentIndex != _tutorialData.length - 1
                           ? GestureDetector(
                               onTap: () {
                                 _controller.nextPage(
@@ -129,26 +132,30 @@ class _TutorialPageState extends State<TutorialPage> {
                                     curve: Curves.easeOut);
                               },
                               child: Text(
-                                'Next',
+                                i18nTranslate(context, "next"),
                                 style: TextStyle(
-                                    fontFamily: 'NotoSansJP',
-                                    fontWeight: FontWeight.w400,
-                                    fontSize: 14,
-                                    color: Color(0xFF000000)),
+                                  fontFamily: 'NotoSansJP',
+                                  fontWeight: FontWeight.w400,
+                                  fontSize: 14,
+                                  color: Color(0xFF000000),
+                                  letterSpacing: calcLetterSpacing(letter: 4),
+                                ),
                               ),
                             )
                           : GestureDetector(
-                              onTap: () {
-                                // ここで別ページに飛ばす
+                              onTap: () async {
                                 print('Start');
+                                await setHasTutorial();
                               },
                               child: Text(
-                                'Start',
+                                i18nTranslate(context, "start"),
                                 style: TextStyle(
-                                    fontFamily: 'NotoSansJP',
-                                    fontWeight: FontWeight.w400,
-                                    fontSize: 14,
-                                    color: Color(0xFF000000)),
+                                  fontFamily: 'NotoSansJP',
+                                  fontWeight: FontWeight.w400,
+                                  fontSize: 14,
+                                  color: Color(0xFF000000),
+                                  letterSpacing: calcLetterSpacing(letter: 4),
+                                ),
                               ),
                             ),
                     ),
@@ -161,65 +168,71 @@ class _TutorialPageState extends State<TutorialPage> {
       ),
     );
   }
+
+  Future<void> setHasTutorial() async {
+    final sharedPreferences = await SharedPreferences.getInstance();
+    sharedPreferences.setBool("has_tutorial", true);
+  }
 }
 
 class Slide extends StatelessWidget {
   final Tutorial data;
-  const Slide({required this.data, Key? key}) : super(key: key);
+  const Slide({required this.data});
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      width: MediaQuery.of(context).size.width,
-      child: Column(
-        children: [
-          Container(
-            width: 270,
-            height: 270,
-            decoration: BoxDecoration(
-              shape: BoxShape.circle,
-              image: DecorationImage(
-                fit: BoxFit.fill,
-                image: AssetImage('assets/images/tutorial/${data.imagePath}'),
-              ),
+    return Column(
+      children: [
+        Container(
+          width: 270,
+          height: 270,
+          decoration: BoxDecoration(
+            shape: BoxShape.circle,
+            image: DecorationImage(
+              fit: BoxFit.fill,
+              image: AssetImage('assets/images/tutorial/${data.imagePath}'),
             ),
           ),
-          SizedBox(
-            height: 48,
-          ),
-          Container(
-            height: 65,
-            alignment: Alignment.center,
-            child: Text(
-              data.title,
-              textAlign: TextAlign.center,
-              style: TextStyle(
-                fontFamily: 'NotoSansJP',
-                fontWeight: FontWeight.w700,
-                fontSize: 18,
-              ),
+        ),
+        SizedBox(
+          height: 48,
+        ),
+        Container(
+          height: 65,
+          alignment: Alignment.center,
+          child: Text(
+            data.title,
+            textAlign: TextAlign.center,
+            style: TextStyle(
+              fontFamily: 'NotoSansJP',
+              fontWeight: FontWeight.w700,
+              fontSize: 18,
+              height: calcFontHeight(fontSize: 18, lineHeight: 32.4),
+              letterSpacing: calcLetterSpacing(letter: 4),
             ),
           ),
-          SizedBox(
-            height: 26,
-          ),
-          Container(
-            height: 67,
-            alignment: Alignment.center,
-            child: Text(
-              data.description,
-              textAlign: TextAlign.center,
-              style: TextStyle(
-                fontWeight: FontWeight.w400,
-                fontSize: 12,
-              ),
+        ),
+        SizedBox(
+          height: 26,
+        ),
+        Container(
+          height: 67,
+          alignment: Alignment.center,
+          child: Text(
+            data.description,
+            textAlign: TextAlign.center,
+            style: TextStyle(
+              fontWeight: FontWeight.w400,
+              fontSize: 12,
+              height: calcFontHeight(fontSize: 12, lineHeight: 22.2),
+              letterSpacing: calcLetterSpacing(letter: 4),
             ),
           ),
-          SizedBox(
-            height: 109,
-          ),
-        ],
-      ),
+        ),
+        SizedBox(
+          height: 109,
+        ),
+      ],
     );
   }
 }
