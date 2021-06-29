a7e3b4517703bc7f0cf93293f2a203dd1fccacf0
machida-yosuke <machida-yosuke@kayac.com>
2021-06-29 19:17:48 +0900

    LoginLoading　Loading

--- minden/lib/features/login/presentation/pages/login_page.dart
+++ minden/lib/features/login/presentation/pages/login_page.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:minden/core/util/bot_toast_helper.dart';
 import 'package:minden/features/login/data/model/user.dart';
 import 'package:minden/features/login/login_api_repository.dart';
 import 'package:minden/features/login/presentation/bloc/login_bloc.dart';
@@ -17,14 +18,20 @@ class _LoginPageState extends State<LoginPage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: BlocProvider(
-        create: (context) => LoginBloc(LoginInitial(), LoginApiRepository()),
+        body: BlocProvider(
+      create: (context) => LoginBloc(LoginInitial(), LoginApiRepository()),
+      child: BlocListener<LoginBloc, LoginState>(
+        listener: (context, state) {
+          if (state is LoginLoading) {
+            Loading.show(context);
+            return;
+          }
+          Loading.hide();
+        },
         child: BlocBuilder<LoginBloc, LoginState>(
           builder: (context, state) {
             if (state is LoginInitial) {
               return buildInitialInput(isError: false);
-            } else if (state is LoginLoading) {
-              return buildLoading();
             } else if (state is LoginLoaded) {
               return buildUserInfo(state.user);
             } else if (state is LoginError) {
@@ -34,17 +41,11 @@ class _LoginPageState extends State<LoginPage> {
           },
         ),
       ),
-    );
+    ));
   }
 
   Widget buildInitialInput({required bool isError}) {
     return LoginInputPage(isError: isError);
-  }
-
-  Widget buildLoading() {
-    return Center(
-      child: CircularProgressIndicator(),
-    );
   }
 
   Widget buildUserInfo(User user) {
