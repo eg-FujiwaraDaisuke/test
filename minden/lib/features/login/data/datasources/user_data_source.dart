3ff0e49e8b386a808e52cbbe6a739d52869ee060
machida-yosuke <machida-yosuke@kayac.com>
2021-07-09 12:52:50 +0900

    api 設定移動

--- minden/lib/features/login/data/datasources/user_data_source.dart
+++ minden/lib/features/login/data/datasources/user_data_source.dart
@@ -1,6 +1,6 @@
 import 'dart:convert';
 import 'package:http/http.dart' as http;
-import 'package:minden/core/env/config.dart';
+import 'package:minden/core/env/api_config.dart';
 import 'package:minden/core/error/exceptions.dart';
 import 'package:minden/features/login/data/model/user_model.dart';
 
@@ -12,35 +12,16 @@ class UserDataSourceImpl implements UserDataSource {
   final http.Client client;
   UserDataSourceImpl({required this.client});
 
-  final _loginApiEnvironment = {
-    Config.kDevFlavor: {
-      'url': 'https://www.dev.minapp.minden.co.jp',
-      'headers': {
-        'content-type': 'application/json',
-      }
-    },
-    Config.kStagingFlavor: {
-      'url': 'https://www.stg.minapp.minden.co.jp',
-      'headers': {
-        'content-type': 'application/json',
-      }
-    },
-    Config.isProduct: {
-      'url': 'https://www.minapp.minden.co.jp',
-      'headers': {
-        'content-type': 'application/json',
-      }
-    }
-  };
-  final _authEndpoint = '/api/v1/auth';
+  final _authPath = '/api/v1/auth';
 
   @override
   Future<UserModel> getLoginUser(String id, String password) async {
-    final env = _loginApiEnvironment[Config.getEnvironmentString()];
     final body = json.encode({'loginId': id, 'password': password});
+    final env = ApiConfig.apiEndpoint();
+
     final response = await client.post(
-      Uri.parse((env?['url'] as String) + _authEndpoint),
-      headers: {'content-type': 'application/json'},
+      Uri.parse((env['url'] as String) + _authPath),
+      headers: env['headers'] as Map<String, String>,
       body: body,
     );
 
