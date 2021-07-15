7656566535381c0ddf47319f84972d4faad248c3
machida-yosuke <machida-yosuke@kayac.com>
2021-07-15 13:59:36 +0900

    crop mock

---
+++ minden/lib/features/camera/pages/camera_mock_page.dart
@@ -0,0 +1,133 @@
+import 'dart:io';
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:image_cropper/image_cropper.dart';
+import 'package:image_picker/image_picker.dart';
+
+class CameraMock extends StatefulWidget {
+  @override
+  _CameraMockState createState() => _CameraMockState();
+}
+
+class _CameraMockState extends State<CameraMock> {
+  File? _image;
+  final _picker = ImagePicker();
+
+  Future _getImage(ImageSource source) async {
+    final PickedFile? pickedFile = await _picker.getImage(source: source);
+    if (pickedFile != null) {
+      final File? cropped = await ImageCropper.cropImage(
+        sourcePath: pickedFile.path,
+        aspectRatio: CropAspectRatio(ratioX: 1, ratioY: 1),
+        compressQuality: 100,
+        maxWidth: 700,
+        maxHeight: 700,
+        compressFormat: ImageCompressFormat.jpg,
+        androidUiSettings: AndroidUiSettings(
+          toolbarColor: Colors.deepOrange,
+          toolbarTitle: 'crop',
+          statusBarColor: Colors.deepOrange.shade900,
+          backgroundColor: Colors.white,
+        ),
+        iosUiSettings: IOSUiSettings(minimumAspectRatio: 1.0),
+      );
+
+      setState(() {
+        _image = cropped == null ? _image : cropped;
+      });
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        body: SafeArea(
+      child: Center(
+        child: Column(children: [
+          _buildImage(),
+          IconButton(
+            icon: Icon(Icons.photo),
+            onPressed: () {
+              _showImageSourceAction();
+            },
+          ),
+        ]),
+      ),
+    ));
+  }
+
+  Widget _buildImage() {
+    return _image == null
+        ? Text('No image selected')
+        : Container(
+            width: 200,
+            height: 200,
+            decoration: BoxDecoration(
+              shape: BoxShape.circle,
+              image: DecorationImage(
+                fit: BoxFit.cover,
+                image: FileImage(_image!),
+              ),
+            ),
+          );
+  }
+
+  void _showImageSourceAction() {
+    Platform.isIOS
+        ? showCupertinoModalPopup(
+            context: context,
+            builder: (context) => CupertinoActionSheet(
+              actions: [
+                CupertinoActionSheetAction(
+                  child: Text('カメラロールから選択する'),
+                  onPressed: () {
+                    Navigator.pop(context);
+                    _getImage(ImageSource.gallery);
+                  },
+                ),
+                CupertinoActionSheetAction(
+                  child: Text('写真を撮影する'),
+                  onPressed: () {
+                    Navigator.pop(context);
+                    _getImage(ImageSource.camera);
+                  },
+                )
+              ],
+              cancelButton: CupertinoActionSheetAction(
+                child: const Text('Cancel'),
+                isDefaultAction: true,
+                onPressed: () {
+                  Navigator.pop(context, 'Cancel');
+                },
+              ),
+            ),
+          )
+        : showModalBottomSheet(
+            context: context,
+            builder: (context) => Wrap(
+              children: [
+                ListTile(
+                  title: Text('カメラロールから選択する'),
+                  onTap: () {
+                    Navigator.pop(context);
+                    _getImage(ImageSource.gallery);
+                  },
+                ),
+                ListTile(
+                  title: Text('写真を撮影する'),
+                  onTap: () {
+                    Navigator.pop(context);
+                    _getImage(ImageSource.camera);
+                  },
+                ),
+                ListTile(
+                  title: Text('Cancel'),
+                  onTap: () {
+                    Navigator.pop(context);
+                  },
+                ),
+              ],
+            ),
+          );
+  }
+}
--- minden/lib/features/debug/debug_page.dart
+++ minden/lib/features/debug/debug_page.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:minden/core/util/no_animation_router.dart';
-import 'package:minden/features/camera/pages/camera_test_page.dart';
+import 'package:minden/features/camera/pages/camera_mock_page.dart';
 import 'package:minden/features/matching/pages/matching_page.dart';
 import 'package:minden/features/startup/presentation/pages/initial_page.dart';
 
@@ -25,7 +25,7 @@ class DebugPage extends StatelessWidget {
                 _generateNavigatorPush(context, 'マイページ - マッチング',
                     (context) => MatchingPage(), '/my_page/matching'),
                 _generateNavigatorPush(context, '画像選択Camera or Gallery',
-                    (context) => CameraTest(), '/camera_test'),
+                    (context) => CameraMock(), '/camera_mock'),
               ],
             ),
           ),
