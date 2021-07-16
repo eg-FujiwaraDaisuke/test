41d01b14c0ed441593bd66886b6d567a956f4e2e
machida-yosuke <machida-yosuke@kayac.com>
2021-07-16 13:38:21 +0900

    debug

--- minden/lib/features/debug/debug_page.dart
+++ minden/lib/features/debug/debug_page.dart
@@ -4,6 +4,8 @@ import 'package:minden/core/util/no_animation_router.dart';
 import 'package:minden/features/camera/pages/camera_mock_page.dart';
 import 'package:minden/features/matching/pages/matching_page.dart';
 import 'package:minden/features/startup/presentation/pages/initial_page.dart';
+import 'package:minden/features/user/presentation/pages/user_page.dart';
+import 'package:minden/features/user/presentation/pages/user_profile_page.dart';
 
 /// デバッグ用画面
 /// デバッグビルド時のみ遷移可能で、各画面への遷移や機能呼び出しを提供する
@@ -22,6 +24,10 @@ class DebugPage extends StatelessWidget {
               children: [
                 _generateNavigatorPush(
                     context, '通常遷移', (context) => InitialPage(), '/'),
+                _generateNavigatorPush(context, 'マイページ - top',
+                    (context) => UserPage(), '/user/matching'),
+                _generateNavigatorPush(context, 'マイページ - プロフィール',
+                    (context) => UserProfilePage(), '/user/profile'),
                 _generateNavigatorPush(context, 'マイページ - マッチング',
                     (context) => MatchingPage(), '/my_page/matching'),
                 _generateNavigatorPush(context, '画像選択Camera or Gallery',
