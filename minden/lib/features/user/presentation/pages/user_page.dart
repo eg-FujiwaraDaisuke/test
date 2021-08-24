1e55e37151f7326df1aedb4d88743b6475d2101a
machida-yosuke <machida-yosuke@kayac.com>
2021-08-24 15:52:55 +0900

    mypage top fix design

--- minden/lib/features/user/presentation/pages/user_page.dart
+++ minden/lib/features/user/presentation/pages/user_page.dart
@@ -1,12 +1,19 @@
+import 'dart:math' as math;
+
 import 'package:flutter/material.dart';
 import 'package:flutter_svg/svg.dart';
 import 'package:minden/core/util/no_animation_router.dart';
 import 'package:minden/core/util/string_util.dart';
-import 'package:minden/features/common/widget/home_mypage_tab_navigation/home_mypage_tab_navigation.dart';
 import 'package:minden/features/user/presentation/pages/profile_damy_data.dart';
 import 'package:minden/features/user/presentation/pages/user_profile_page.dart';
 import 'package:minden/features/user/presentation/pages/user_thanks_message_page.dart';
+
 import '../../../../utile.dart';
+
+enum MenuType {
+  common,
+  message,
+}
 
 class UserPage extends StatelessWidget {
 // 確認用仮データ
@@ -16,10 +23,12 @@ class UserPage extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        backgroundColor: Colors.white,
+        backgroundColor: Colors.transparent,
         elevation: 0.0,
+        centerTitle: true,
         title: Text(
           i18nTranslate(context, 'user_mypage'),
+          textAlign: TextAlign.center,
           style: TextStyle(
             color: Colors.black,
             fontSize: 16,
@@ -29,20 +38,44 @@ class UserPage extends StatelessWidget {
           ),
         ),
       ),
-      body: Container(
-        color: Colors.white,
-        child: Column(
-          children: [
-            _UserProfile(
-              icon: data.icon,
-              name: data.name,
-              contractor: data.contractor,
+      body: SafeArea(
+        child: SingleChildScrollView(
+          child: Container(
+            child: Column(
+              children: [
+                SizedBox(
+                  height: 43,
+                ),
+                Stack(
+                  clipBehavior: Clip.none,
+                  children: [
+                    Positioned(
+                      top: -500,
+                      left: -70,
+                      child: Container(
+                        width: 561,
+                        height: 561,
+                        decoration: BoxDecoration(
+                          color: Color(0xFFFFFB92),
+                          shape: BoxShape.circle,
+                        ),
+                      ),
+                    ),
+                    Positioned(
+                      child: _UserProfile(
+                        icon: data.icon,
+                        name: data.name,
+                      ),
+                    ),
+                  ],
+                ),
+                SizedBox(
+                  height: 61,
+                ),
+                _MenuListView(),
+              ],
             ),
-            SizedBox(
-              height: 39,
-            ),
-            _MenuListView(),
-          ],
+          ),
         ),
       ),
     );
@@ -51,56 +84,41 @@ class UserPage extends StatelessWidget {
 
 class _UserProfile extends StatelessWidget {
   final String name;
-  final String contractor;
+
   final String icon;
   _UserProfile({
     required this.name,
-    required this.contractor,
     required this.icon,
   }) : super();
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      child: Row(
+    return Center(
+      child: Column(
         children: [
           Container(
-            margin: EdgeInsets.only(left: 22),
-            width: 64,
-            height: 64,
+            width: 99,
+            height: 99,
             decoration: BoxDecoration(
-              color: Colors.red,
+              color: Color(0xFFFFFB92),
               shape: BoxShape.circle,
+              border: Border.all(
+                width: 3,
+                color: Color(0xFFFF8C00),
+              ),
             ),
           ),
           SizedBox(
-            width: 20,
+            width: 16,
           ),
-          Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: [
-              Text(
-                contractor,
-                style: TextStyle(
-                  color: Colors.black,
-                  fontSize: 16,
-                  fontFamily: 'NotoSansJP',
-                  fontWeight: FontWeight.w500,
-                ),
-              ),
-              SizedBox(
-                height: 5,
-              ),
-              Text(
-                name,
-                style: TextStyle(
-                  color: Color(0xFF7C7C7C),
-                  fontSize: 14,
-                  fontFamily: 'NotoSansJP',
-                  fontWeight: FontWeight.w400,
-                ),
-              ),
-            ],
+          Text(
+            name,
+            style: TextStyle(
+              color: Color(0xFF575292),
+              fontSize: 18,
+              fontFamily: 'NotoSansJP',
+              fontWeight: FontWeight.w700,
+            ),
           )
         ],
       ),
@@ -112,14 +130,12 @@ class _Menu {
   final String title;
   final String icon;
   final NoAnimationMaterialPageRoute? route;
-  final bool hasUnreadNotice;
-  final bool isAccordion;
+  final MenuType type;
   _Menu({
     required this.title,
     required this.icon,
-    this.route,
-    this.hasUnreadNotice = false,
-    this.isAccordion = false,
+    required this.route,
+    required this.type,
   });
 }
 
@@ -128,86 +144,79 @@ class _MenuListView extends StatelessWidget {
   Widget build(BuildContext context) {
     final _menuList = [
       _Menu(
-        // TODO 契約内容はタップしたらアコーディオンで契約内容が出現する
-        title: i18nTranslate(context, 'user_menu_contract'),
-        icon: 'contract',
-        isAccordion: true,
-      ),
+          title: i18nTranslate(context, 'user_menu_select_plant'),
+          icon: 'select_plant',
+          // TODO routeは仮
+          route: NoAnimationMaterialPageRoute(
+            builder: (context) => UserProfilePage(),
+            settings: RouteSettings(name: "/user/profile"),
+          ),
+          type: MenuType.common),
       _Menu(
-        title: i18nTranslate(context, 'user_menu_select_plant'),
-        icon: 'select_plant',
-        // TODO routeは仮
-        route: NoAnimationMaterialPageRoute(
-          builder: (context) => UserProfilePage(),
-          settings: RouteSettings(name: "/user/profile"),
-        ),
-      ),
+          title: i18nTranslate(context, 'user_menu_profile'),
+          icon: 'person',
+          route: NoAnimationMaterialPageRoute(
+            builder: (context) => UserProfilePage(),
+            settings: RouteSettings(name: "/user/profile"),
+          ),
+          type: MenuType.common),
       _Menu(
-        title: i18nTranslate(context, 'user_menu_profile'),
-        icon: 'person',
-        route: NoAnimationMaterialPageRoute(
-          builder: (context) => UserProfilePage(),
-          settings: RouteSettings(name: "/user/profile"),
-        ),
-      ),
+          title: i18nTranslate(context, 'user_menu_thanks_message'),
+          icon: 'message',
+          route: NoAnimationMaterialPageRoute(
+            builder: (context) => UserThanksMessagePage(),
+            settings: RouteSettings(name: "/user/thanksMessage"),
+          ),
+          type: MenuType.message),
       _Menu(
-        title: i18nTranslate(context, 'user_menu_thanks_message'),
-        icon: 'message',
-        hasUnreadNotice: true,
-        route: NoAnimationMaterialPageRoute(
-          builder: (context) => UserThanksMessagePage(),
-          settings: RouteSettings(name: "/user/thanksMessage"),
-        ),
-      ),
+          title: i18nTranslate(context, 'user_menu_contact'),
+          icon: 'contact',
+          // TODO routeは仮
+          route: NoAnimationMaterialPageRoute(
+            builder: (context) => UserProfilePage(),
+            settings: RouteSettings(name: "/user/profile"),
+          ),
+          type: MenuType.common),
       _Menu(
-        title: i18nTranslate(context, 'user_menu_contact'),
-        icon: 'contact',
-        // TODO routeは仮
-        route: NoAnimationMaterialPageRoute(
-          builder: (context) => UserProfilePage(),
-          settings: RouteSettings(name: "/user/profile"),
-        ),
-      ),
-      _Menu(
-        title: i18nTranslate(context, 'user_menu_logout'),
-        icon: 'logout',
-        // TODO routeは仮
-        route: NoAnimationMaterialPageRoute(
-          builder: (context) => UserProfilePage(),
-          settings: RouteSettings(name: "/user/profile"),
-        ),
-      ),
+          title: i18nTranslate(context, 'user_menu_logout'),
+          icon: 'logout',
+          // TODO routeは仮
+          route: NoAnimationMaterialPageRoute(
+            builder: (context) => UserProfilePage(),
+            settings: RouteSettings(name: "/user/profile"),
+          ),
+          type: MenuType.common),
     ];
     return Container(
       child: Column(
-        children: _menuList
-            .map(
-              (e) => _MenuItem(
-                title: e.title,
-                icon: e.icon,
-                route: e.route,
-                hasUnreadNotice: e.hasUnreadNotice,
-                isAccordion: e.isAccordion,
-              ),
-            )
-            .toList(),
+        children: _menuList.map((menu) {
+          if (menu.type == MenuType.message) {
+            return _MenuMessageItem(
+              title: menu.title,
+              icon: menu.icon,
+              route: menu.route,
+            );
+          }
+          return _MenuItem(
+            title: menu.title,
+            icon: menu.icon,
+            route: menu.route,
+          );
+        }).toList(),
       ),
     );
   }
 }
 
 class _MenuItem extends StatelessWidget {
-  final title;
-  final icon;
-  final route;
-  final bool hasUnreadNotice;
-  final bool isAccordion;
+  final String title;
+  final String icon;
+  final NoAnimationMaterialPageRoute? route;
+
   const _MenuItem({
     required this.title,
     required this.icon,
     required this.route,
-    this.hasUnreadNotice = false,
-    this.isAccordion = false,
   }) : super();
 
   @override
@@ -215,9 +224,65 @@ class _MenuItem extends StatelessWidget {
     return GestureDetector(
       behavior: HitTestBehavior.opaque,
       onTap: () {
-        print(route);
         if (route != null) {
-          Navigator.pushReplacement(context, route);
+          Navigator.pushReplacement(context, route!);
+        }
+      },
+      child: Container(
+        padding: EdgeInsets.symmetric(horizontal: 22),
+        height: 56,
+        width: MediaQuery.of(context).size.width,
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.start,
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Container(
+              child: Row(
+                children: [
+                  SvgPicture.asset(
+                    'assets/images/user/$icon.svg',
+                    color: Color(0xFF575292),
+                  ),
+                  const SizedBox(width: 17.5),
+                  Text(
+                    title,
+                    style: TextStyle(
+                      color: Color(0xFF575292),
+                      fontSize: 12,
+                      fontFamily: 'NotoSansJP',
+                      fontWeight: FontWeight.w700,
+                      letterSpacing: calcLetterSpacing(letter: 0.5),
+                    ),
+                  ),
+                ],
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+class _MenuMessageItem extends StatelessWidget {
+  final String title;
+  final String icon;
+  final NoAnimationMaterialPageRoute? route;
+  final bool hasUnreadNotice = true;
+
+  const _MenuMessageItem({
+    required this.title,
+    required this.icon,
+    required this.route,
+  }) : super();
+
+  @override
+  Widget build(BuildContext context) {
+    return GestureDetector(
+      behavior: HitTestBehavior.opaque,
+      onTap: () {
+        if (route != null) {
+          Navigator.pushReplacement(context, route!);
         }
       },
       child: Container(
@@ -235,14 +300,15 @@ class _MenuItem extends StatelessWidget {
                     clipBehavior: Clip.none,
                     children: [
                       Positioned(
-                        child: SvgPicture.asset('assets/images/user/$icon.svg'),
+                        child: SvgPicture.asset(
+                          'assets/images/user/$icon.svg',
+                          color: Color(0xFF575292),
+                        ),
                       ),
                       Positioned(
                         right: -6,
                         top: -3,
                         child: Opacity(
-                          //アプリがforegroundになった際に、応援メッセージ履歴取得APIにて既読メッセージの有無を取得する。
-                          //既読メッセージが存在している場合、マイページタブ、メッセージラベルに未読バッジを表示する。
                           opacity: hasUnreadNotice ? 1 : 0,
                           child: Container(
                             width: 14,
@@ -264,17 +330,14 @@ class _MenuItem extends StatelessWidget {
                   Text(
                     title,
                     style: TextStyle(
-                      color: Colors.black,
+                      color: Color(0xFF575292),
                       fontSize: 12,
                       fontFamily: 'NotoSansJP',
-                      fontWeight: FontWeight.w400,
+                      fontWeight: FontWeight.w700,
                       letterSpacing: calcLetterSpacing(letter: 0.5),
                     ),
                   ),
                   SizedBox(width: 22),
-                  isAccordion ? Icon(Icons.keyboard_arrow_right) : Container(),
-                  //アプリがforegroundになった際に、応援メッセージ履歴取得APIにて既読メッセージの有無を取得する。
-                  //既読メッセージが存在している場合、マイページタブ、メッセージラベルに未読バッジを表示する。
                   hasUnreadNotice
                       ? Flexible(
                           child: Text(
