6e51d8f546280d0451589706f90a8c4db678c962
machida-yosuke <machida-yosuke@kayac.com>
2021-09-08 12:50:31 +0900

    背景色

--- minden/lib/features/forgot_password/pages/forgot_password_page.dart
+++ minden/lib/features/forgot_password/pages/forgot_password_page.dart
@@ -29,7 +29,6 @@ class _ForgotPasswordPageState extends State<ForgotPasswordPage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: Color(0xFFFAF9F2),
       appBar: AppBar(
         backgroundColor: Colors.transparent,
         elevation: 0.0,
