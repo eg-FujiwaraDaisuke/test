24fce94fe7b4be11ff09377c83077f7fdaee666e
machida-yosuke <machida-yosuke@kayac.com>
2021-09-10 15:25:07 +0900

    dialog表示用のデバックページ

---
+++ minden/lib/features/support_plant/presentation/support_plant_dialog_debug_page.dart
@@ -0,0 +1,49 @@
+import 'package:flutter/material.dart';
+import 'package:minden/features/support_plant/domain/entities/participant.dart';
+import 'package:minden/features/support_plant/presentation/support_participants_dialog.dart';
+import 'package:minden/features/support_plant/presentation/support_plant_dialog.dart';
+
+class SupportPlantDialogDebugPage extends StatelessWidget {
+  const SupportPlantDialogDebugPage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: SafeArea(
+        child: Column(
+          children: [
+            ElevatedButton(
+              child: Text('契約件数1/応援0の場合'),
+              onPressed: () {
+                SupportPlantDialog(context: context).showDialog();
+              },
+            ),
+            ElevatedButton(
+              child: Text('契約件数1/応援1の場合'),
+              onPressed: () {
+                SupportPlantDialog(context: context).showDialog();
+              },
+            ),
+            ElevatedButton(
+              child: Text('契約件数2/応援2の場合'),
+              onPressed: () {
+                SupportPlantDialog(context: context).showDialog();
+              },
+            ),
+            SizedBox(
+              height: 10,
+            ),
+            ElevatedButton(
+              child: Text('サポートしてる人たち'),
+              onPressed: () {
+                SupportParticipantsDialog(
+                        context: context, participants: participantsDammyData)
+                    .showDialog();
+              },
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
