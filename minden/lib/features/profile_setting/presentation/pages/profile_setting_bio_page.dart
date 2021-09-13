f13af222e55e4bd9964b81a43a6d14edcb8f8959
machida-yosuke <machida-yosuke@kayac.com>
2021-09-13 15:06:17 +0900

    botton

--- minden/lib/features/profile_setting/presentation/pages/profile_setting_bio_page.dart
+++ minden/lib/features/profile_setting/presentation/pages/profile_setting_bio_page.dart
@@ -3,8 +3,8 @@ import 'package:flutter_svg/svg.dart';
 import 'package:http/http.dart' as http;
 import 'package:minden/core/util/bot_toast_helper.dart';
 import 'package:minden/core/util/string_util.dart';
-import 'package:minden/features/common/widget/button/botton_size.dart';
 import 'package:minden/features/common/widget/button/button.dart';
+import 'package:minden/features/common/widget/button/button_size.dart';
 import 'package:minden/features/profile_setting/presentation/pages/profile_setting_tags_page.dart';
 import 'package:minden/features/user/data/datasources/profile_datasource.dart';
 import 'package:minden/features/user/data/repositories/profile_repository_impl.dart';
@@ -102,10 +102,10 @@ class _ProfileSettingBioPageState extends State<ProfileSettingBioPage> {
                       _inputBio = value;
                     }),
                 const SizedBox(height: 126),
-                Botton(
+                Button(
                   onTap: _next,
                   text: i18nTranslate(context, 'profile_setting_next'),
-                  size: BottonSize.S,
+                  size: ButtonSize.S,
                 ),
               ],
             ),
