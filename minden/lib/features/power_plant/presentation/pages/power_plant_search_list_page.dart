1b0a05e4b0e233c5a66bd0571581cc849a6fe859
machida-yosuke <machida-yosuke@kayac.com>
2021-09-22 20:40:17 +0900

    検索数

--- minden/lib/features/power_plant/presentation/pages/power_plant_search_list_page.dart
+++ minden/lib/features/power_plant/presentation/pages/power_plant_search_list_page.dart
@@ -6,6 +6,7 @@ import 'package:minden/core/util/bot_toast_helper.dart';
 import 'package:minden/features/common/widget/tag/tag_list_item.dart';
 import 'package:minden/features/power_plant/data/datasources/power_plant_data_source.dart';
 import 'package:minden/features/power_plant/data/repositories/power_plant_repository_impl.dart';
+import 'package:minden/features/power_plant/domain/entities/power_plant.dart';
 import 'package:minden/features/power_plant/domain/usecase/power_plant_usecase.dart';
 import 'package:minden/features/power_plant/presentation/bloc/power_plant_bloc.dart';
 import 'package:minden/features/power_plant/presentation/bloc/power_plant_event.dart';
@@ -14,10 +15,42 @@ import 'package:minden/features/power_plant/presentation/pages/power_plant_list_
 import 'package:minden/features/profile_setting/domain/entities/tag.dart';
 import 'package:minden/utile.dart';
 
-class PowerPlantSearchListPage extends StatelessWidget {
+class PowerPlantSearchListPage extends StatefulWidget {
   const PowerPlantSearchListPage({required this.selectTag, Key? key})
       : super(key: key);
   final Tag selectTag;
+
+  @override
+  _PowerPlantSearchListPageState createState() =>
+      _PowerPlantSearchListPageState();
+}
+
+class _PowerPlantSearchListPageState extends State<PowerPlantSearchListPage> {
+  late GetPowerPlantsBloc _bloc;
+
+  @override
+  void initState() {
+    super.initState();
+
+    _bloc = GetPowerPlantsBloc(
+      const PowerPlantStateInitial(),
+      GetPowerPlants(
+        PowerPlantRepositoryImpl(
+          powerPlantDataSource: PowerPlantDataSourceImpl(
+            client: http.Client(),
+          ),
+        ),
+      ),
+    );
+
+    _bloc.add(GetPowerPlantsEvent(tagId: widget.selectTag.tagId.toString()));
+  }
+
+  @override
+  void dispose() {
+    _bloc.close();
+    super.dispose();
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -51,206 +84,177 @@ class PowerPlantSearchListPage extends StatelessWidget {
       ),
       body: SafeArea(
         child: SingleChildScrollView(
-          child: Container(
-            color: const Color(0xFFFAF9F8),
-            child: Center(
-              child: Column(
-                children: [
-                  const SizedBox(height: 17),
-                  Container(
-                    width: 340,
-                    height: 44,
-                    decoration: BoxDecoration(
-                      borderRadius: BorderRadius.circular(40),
-                      color: Colors.white,
-                      border: Border.all(
-                        color: const Color(0xFFE2E2E2),
-                      ),
-                    ),
-                    child: Container(
-                      padding: const EdgeInsets.only(left: 5, right: 24),
-                      child: Row(
-                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            child: BlocProvider.value(
+          value: _bloc,
+          child: BlocListener<GetPowerPlantsBloc, PowerPlantState>(
+            listener: (context, state) {
+              if (state is PowerPlantLoading) {
+                Loading.show(context);
+                return;
+              }
+              Loading.hide();
+            },
+            child: BlocBuilder<GetPowerPlantsBloc, PowerPlantState>(
+              builder: (context, state) {
+                if (state is PowerPlantsLoaded) {
+                  return Container(
+                    color: const Color(0xFFFAF9F8),
+                    child: Center(
+                      child: Column(
                         children: [
-                          Stack(
-                            clipBehavior: Clip.antiAlias,
-                            children: [
-                              Container(
-                                width: 150,
+                          const SizedBox(height: 17),
+                          Container(
+                            width: 340,
+                            height: 44,
+                            decoration: BoxDecoration(
+                              borderRadius: BorderRadius.circular(40),
+                              color: Colors.white,
+                              border: Border.all(
+                                color: const Color(0xFFE2E2E2),
                               ),
-                              Positioned(
-                                top: 4,
-                                child: TagListItem(
-                                  tag: selectTag,
-                                  onSelect: (tag) {},
-                                  isSelected: true,
-                                ),
-                              ),
-                              Container(
-                                width: 150,
-                                decoration: BoxDecoration(
-                                  gradient: LinearGradient(
-                                    begin: FractionalOffset.topLeft,
-                                    end: FractionalOffset.topRight,
-                                    colors: [
-                                      const Color(0xffffffff).withOpacity(0),
-                                      const Color(0xffffffff).withOpacity(1),
-                                    ],
-                                    stops: const [
-                                      0.6,
-                                      1.0,
+                            ),
+                            child: Container(
+                              padding:
+                                  const EdgeInsets.only(left: 5, right: 24),
+                              child: Row(
+                                mainAxisAlignment:
+                                    MainAxisAlignment.spaceBetween,
+                                children: [
+                                  Stack(
+                                    clipBehavior: Clip.antiAlias,
+                                    children: [
+                                      Container(
+                                        width: 150,
+                                      ),
+                                      Positioned(
+                                        top: 4,
+                                        child: TagListItem(
+                                          tag: widget.selectTag,
+                                          onSelect: (tag) {},
+                                          isSelected: true,
+                                        ),
+                                      ),
+                                      Container(
+                                        width: 150,
+                                        decoration: BoxDecoration(
+                                          gradient: LinearGradient(
+                                            begin: FractionalOffset.topLeft,
+                                            end: FractionalOffset.topRight,
+                                            colors: [
+                                              const Color(0xffffffff)
+                                                  .withOpacity(0),
+                                              const Color(0xffffffff)
+                                                  .withOpacity(1),
+                                            ],
+                                            stops: const [
+                                              0.6,
+                                              1.0,
+                                            ],
+                                          ),
+                                        ),
+                                      ),
                                     ],
                                   ),
-                                ),
+                                  Row(
+                                    children: [
+                                      Center(
+                                        child: SvgPicture.asset(
+                                          'assets/images/power_plant/search.svg',
+                                          width: 16,
+                                          height: 16,
+                                          color: const Color(0xFFA7A7A7),
+                                        ),
+                                      ),
+                                      const SizedBox(
+                                        width: 3,
+                                      ),
+                                      Text(
+                                        '検索結果･･･',
+                                        style: TextStyle(
+                                          fontSize: 13,
+                                          fontFamily: 'NotoSansJP',
+                                          fontWeight: FontWeight.w700,
+                                          color: const Color(0xFFA7A7A7),
+                                          letterSpacing:
+                                              calcLetterSpacing(letter: 4),
+                                        ),
+                                      ),
+                                      Text(
+                                        state.powerPlants.powerPlants.length
+                                            .toString(),
+                                        style: TextStyle(
+                                          fontSize: 20,
+                                          fontFamily: 'NotoSansJP',
+                                          fontWeight: FontWeight.w700,
+                                          color: const Color(0xFF575292),
+                                          letterSpacing:
+                                              calcLetterSpacing(letter: 4),
+                                        ),
+                                      ),
+                                      Text(
+                                        '件',
+                                        style: TextStyle(
+                                          fontSize: 13,
+                                          fontFamily: 'NotoSansJP',
+                                          fontWeight: FontWeight.w700,
+                                          color: const Color(0xFF575292),
+                                          letterSpacing:
+                                              calcLetterSpacing(letter: 4),
+                                        ),
+                                      )
+                                    ],
+                                  )
+                                ],
                               ),
-                            ],
+                            ),
                           ),
-                          Row(
-                            children: [
-                              Center(
-                                child: SvgPicture.asset(
-                                  'assets/images/power_plant/search.svg',
-                                  width: 16,
-                                  height: 16,
-                                  color: const Color(0xFFA7A7A7),
-                                ),
-                              ),
-                              const SizedBox(
-                                width: 3,
-                              ),
-                              Text(
-                                '検索結果･･･',
-                                style: TextStyle(
-                                  fontSize: 13,
-                                  fontFamily: 'NotoSansJP',
-                                  fontWeight: FontWeight.w700,
-                                  color: const Color(0xFFA7A7A7),
-                                  letterSpacing: calcLetterSpacing(letter: 4),
-                                ),
-                              ),
-                              Text(
-                                '３',
-                                style: TextStyle(
-                                  fontSize: 20,
-                                  fontFamily: 'NotoSansJP',
-                                  fontWeight: FontWeight.w700,
-                                  color: const Color(0xFF575292),
-                                  letterSpacing: calcLetterSpacing(letter: 4),
-                                ),
-                              ),
-                              Text(
-                                '件',
-                                style: TextStyle(
-                                  fontSize: 13,
-                                  fontFamily: 'NotoSansJP',
-                                  fontWeight: FontWeight.w700,
-                                  color: const Color(0xFF575292),
-                                  letterSpacing: calcLetterSpacing(letter: 4),
-                                ),
-                              )
-                            ],
-                          )
+                          const SizedBox(height: 43),
+                          Text(
+                            'このタグを選んだ人が応援している発電所',
+                            style: TextStyle(
+                                fontSize: 16,
+                                fontFamily: 'NotoSansJP',
+                                fontWeight: FontWeight.w700,
+                                color: const Color(0xFF575292),
+                                letterSpacing: calcLetterSpacing(letter: 4)),
+                          ),
+                          const SizedBox(height: 31),
+                          _PowerPlantSearchList(
+                              powerPlants: state.powerPlants.powerPlants),
                         ],
                       ),
                     ),
-                  ),
-                  const SizedBox(height: 43),
-                  Text(
-                    'このタグを選んだ人が応援している発電所',
-                    style: TextStyle(
-                        fontSize: 16,
-                        fontFamily: 'NotoSansJP',
-                        fontWeight: FontWeight.w700,
-                        color: const Color(0xFF575292),
-                        letterSpacing: calcLetterSpacing(letter: 4)),
-                  ),
-                  const SizedBox(height: 31),
-                  _PowerPlantSearchList(
-                    tagId: selectTag.tagId.toString(),
-                  ),
-                ],
-              ),
+                  );
+                }
+                return Container();
+              },
             ),
           ),
-        ),
+        )),
       ),
     );
   }
 }
 
 /// 発電所一覧
-class _PowerPlantSearchList extends StatefulWidget {
-  const _PowerPlantSearchList({required this.tagId});
-
-  final String tagId;
-
-  @override
-  State<StatefulWidget> createState() {
-    return _PowerPlantSearchListState();
-  }
-}
-
-class _PowerPlantSearchListState extends State<_PowerPlantSearchList> {
-  late GetPowerPlantsBloc _bloc;
-
-  @override
-  void initState() {
-    super.initState();
-
-    _bloc = GetPowerPlantsBloc(
-      const PowerPlantStateInitial(),
-      GetPowerPlants(
-        PowerPlantRepositoryImpl(
-          powerPlantDataSource: PowerPlantDataSourceImpl(
-            client: http.Client(),
-          ),
-        ),
-      ),
-    );
-
-    _bloc.add(GetPowerPlantsEvent(tagId: widget.tagId));
-  }
-
-  @override
-  void dispose() {
-    _bloc.close();
-    super.dispose();
-  }
-
+class _PowerPlantSearchList extends StatelessWidget {
+  _PowerPlantSearchList({required this.powerPlants});
+  final List<PowerPlant> powerPlants;
   @override
   Widget build(BuildContext context) {
-    return BlocProvider.value(
-      value: _bloc,
-      child: BlocListener<GetPowerPlantsBloc, PowerPlantState>(
-        listener: (context, state) {
-          if (state is PowerPlantLoading) {
-            Loading.show(context);
-            return;
-          }
-          Loading.hide();
-        },
-        child: BlocBuilder<GetPowerPlantsBloc, PowerPlantState>(
-          builder: (context, state) {
-            if (state is PowerPlantsLoaded) {
-              var index = 0;
-              final plants = [];
-              state.powerPlants.powerPlants.forEach((element) {
-                final direction = searchDirectionByIndex(index);
-                index++;
-                plants.add(PowerPlantListItem(
-                  key: ValueKey(element.plantId),
-                  powerPlant: element,
-                  direction: direction,
-                ));
-              });
-              return Column(
-                children: [...plants],
-              );
-            }
-            return Container();
-          },
-        ),
-      ),
+    var index = 0;
+    final plants = [];
+    powerPlants.forEach((element) {
+      final direction = searchDirectionByIndex(index);
+      index++;
+      plants.add(PowerPlantListItem(
+        key: ValueKey(element.plantId),
+        powerPlant: element,
+        direction: direction,
+      ));
+    });
+    return Column(
+      children: [...plants],
     );
   }
 
