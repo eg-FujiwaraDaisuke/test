9f4aa7de67b25432ba6cb1c84970dfaddd4c7c3a
machida-yosuke <machida-yosuke@kayac.com>
2021-09-30 17:07:31 +0900

    FB修正

--- minden/lib/features/home/presentation/pages/home_page.dart
+++ minden/lib/features/home/presentation/pages/home_page.dart
@@ -1,8 +1,6 @@
 import 'package:firebase_messaging/firebase_messaging.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:minden/core/ext/logger_ext.dart';
-import 'package:minden/core/util/bot_toast_helper.dart';
 import 'package:minden/features/common/widget/home_mypage_tab_navigation/home_mypage_tab.dart';
 import 'package:minden/features/common/widget/home_mypage_tab_navigation/home_mypage_tab_navigation.dart';
 import 'package:minden/features/common/widget/home_mypage_tab_navigation/tab_navigator.dart';
@@ -11,14 +9,11 @@ import 'package:minden/features/fcm/data/datasources/fcm_token_data_source.dart'
 import 'package:minden/features/fcm/data/repositories/fcm_token_repository_impl.dart';
 import 'package:minden/features/fcm/domain/usecases/update_fcm_token.dart';
 import 'package:minden/features/fcm/pages/bloc/fcm_bloc.dart';
-import 'package:minden/features/login/presentation/bloc/logout_bloc.dart';
-import 'package:minden/features/login/presentation/bloc/logout_event.dart';
-import 'package:minden/features/login/presentation/pages/login_page.dart';
 import 'package:minden/injection_container.dart';
 import 'package:http/http.dart' as http;
 
 // FCMプッシュ通知の遷移周りの初期化を行っています。
-//bottomNavigationBarの出し分けを行います
+// bottomNavigationBarの出し分けを行います
 
 class HomePage extends StatefulWidget {
   @override
@@ -41,7 +36,12 @@ class _HomePageState extends State<HomePage> {
 
   void _postToken(String? token) {
     logD('FCM token : $token');
-    _bloc.add(UpdateFcmTokenEvent(token ?? ''));
+
+    if (token == null) {
+      return;
+    }
+
+    _bloc.add(UpdateFcmTokenEvent(token));
   }
 
   @override
@@ -59,20 +59,6 @@ class _HomePageState extends State<HomePage> {
       ),
     );
 
-    _bloc.stream.listen((event) async {
-      if (event is FcmStateError) {
-        logD(event.toString());
-        if (event.needLogin) {
-          BlocProvider.of<LogoutBloc>(context).add(LogoutEvent());
-          await Navigator.of(context, rootNavigator: true).pushAndRemoveUntil(
-              MaterialPageRoute(
-                builder: (context) => LoginPage(),
-              ),
-              (_) => false);
-        }
-      }
-    });
-
     si<FirebaseMessaging>().getToken().then(_postToken);
     si<FirebaseMessaging>().onTokenRefresh.listen(_postToken);
 
@@ -80,7 +66,6 @@ class _HomePageState extends State<HomePage> {
     //TODO 通知をタップしたらメッセージページに遷移させたい
     //ターミネイト状態でプッシュ通知メッセージからアプリを起動した場合の遷移
     si<FirebaseMessaging>().getInitialMessage().then((RemoteMessage? message) {
-      logD('===========================ターミネイト状態');
       logD('${message}');
       if (message != null) {
         Navigator.of(context).push(
@@ -99,7 +84,6 @@ class _HomePageState extends State<HomePage> {
     // バックグラウンド状態でプッシュ通知メッセージからアプリを起動した場合の遷移
     FirebaseMessaging.onMessageOpenedApp.listen((RemoteMessage message) {
       logD('${message}');
-      logD('=========================== バックグラウンド状態');
       Navigator.of(context).push(
         MaterialPageRoute(
           settings: RouteSettings(
@@ -124,34 +108,17 @@ class _HomePageState extends State<HomePage> {
     return WillPopScope(
       onWillPop: () async =>
           !await _navigatorKeys[_currentTab]!.currentState!.maybePop(),
-      child: BlocProvider.value(
-        value: _bloc,
-        child: BlocListener<UpdateFcmTokenBloc, FcmState>(
-            listener: (context, state) {
-          if (state is FcmStateUpdateing) {
-            Loading.show(context);
-            return;
-          }
-          Loading.hide();
-        }, child: BlocBuilder<UpdateFcmTokenBloc, FcmState>(
-          builder: (context, state) {
-            if (state is FcmStateUpdated) {
-              return Scaffold(
-                body: Stack(
-                  children: [
-                    _buildOffstageNavigator(TabItem.home),
-                    _buildOffstageNavigator(TabItem.mypage)
-                  ],
-                ),
-                bottomNavigationBar: HomeMypageTabNavigation(
-                  currentTab: _currentTab,
-                  onSelectTab: _selectTab,
-                ),
-              );
-            }
-            return Container();
-          },
-        )),
+      child: Scaffold(
+        body: Stack(
+          children: [
+            _buildOffstageNavigator(TabItem.home),
+            _buildOffstageNavigator(TabItem.mypage)
+          ],
+        ),
+        bottomNavigationBar: HomeMypageTabNavigation(
+          currentTab: _currentTab,
+          onSelectTab: _selectTab,
+        ),
       ),
     );
   }
