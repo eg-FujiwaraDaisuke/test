7933ae5307dbbb982e8d8f8348610ed47e51f07d
daimarom <zonbidai@gmail.com>
2022-03-27 00:34:53 +0900

    feat: Tab in TabでrootのScreenViewEventを送信可能にする

--- minden/lib/features/power_plant/presentation/pages/power_plant_page.dart
+++ minden/lib/features/power_plant/presentation/pages/power_plant_page.dart
@@ -1,8 +1,11 @@
+import 'package:firebase_analytics/firebase_analytics.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:flutter_hooks/flutter_hooks.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
+import 'package:minden/core/firebase/analytics_factory.dart';
 import 'package:minden/core/firebase/dynamic_links_route_mapper.dart';
+import 'package:minden/core/hook/use_logger.dart';
 import 'package:minden/core/provider/firebase_dynamic_links_provider.dart';
 import 'package:minden/features/power_plant/presentation/pages/power_plant_detail_page.dart';
 import 'package:minden/features/power_plant/presentation/pages/power_plant_list_page.dart';
@@ -13,10 +16,12 @@ class PowerPlantHomeTabData {
   PowerPlantHomeTabData({
     required this.tabName,
     required this.tabPage,
+    required this.tabPageRoute,
   });
 
   late final String tabName;
   late final WidgetBuilder tabPage;
+  late final String tabPageRoute;
 }
 
 /// ホームタブ
@@ -27,27 +32,37 @@ class PowerPlantHomePage extends StatefulWidget {
   _PowerPlantHomePageState createState() => _PowerPlantHomePageState();
 }
 
-/// 発電所を探すタブの、探し方選択まではタブ内で画面遷移を行う
-final GlobalKey<NavigatorState> _searchPowerPlantNavKey =
-    GlobalKey<NavigatorState>();
-
-class _PowerPlantHomePageState extends State<PowerPlantHomePage> {
+class _PowerPlantHomePageState extends State<PowerPlantHomePage>
+    with SingleTickerProviderStateMixin {
   late TransitionScreenBloc _transitionScreenBloc;
 
   final tabs = [
     PowerPlantHomeTabData(
       tabName: '発電所一覧',
       tabPage: (_) => PowerPlantList(),
+      tabPageRoute: PowerPlantList.routeName,
     ),
     PowerPlantHomeTabData(
       tabName: '発電所を探す',
       tabPage: (_) => const PowerPlantSearchMenu(),
+      tabPageRoute: PowerPlantSearchMenu.routeName,
     ),
   ];
+
+  late TabController _tabController;
 
   @override
   void initState() {
     super.initState();
+
+    _tabController = TabController(length: tabs.length, vsync: this);
+    _tabController.addListener(() {
+      // NOTE: 仕様により、タブ切り替えアニメーション中と、完了時の通知が2回飛んでくる
+      // よって完了時のみ検知する
+      if (!_tabController.indexIsChanging) {
+        sendScreenTabViewEvent(_tabController.index);
+      }
+    });
 
     _transitionScreenBloc = BlocProvider.of<TransitionScreenBloc>(context);
     _transitionScreenBloc.stream.listen((event) {
@@ -63,6 +78,10 @@ class _PowerPlantHomePageState extends State<PowerPlantHomePage> {
 
   @override
   Widget build(BuildContext context) {
+    // 画面表示時の、初回表示タブについて、ScreenViewEventを送信する
+    // NOTE: BottomNavigation切り替えの場合、0とは限らない
+    sendScreenTabViewEvent(_tabController.index);
+
     return DefaultTabController(
       length: tabs.length,
       child: Scaffold(
@@ -80,6 +99,7 @@ class _PowerPlantHomePageState extends State<PowerPlantHomePage> {
                           textAlign: TextAlign.center,
                         ))
                     .toList(),
+                controller: _tabController,
               ),
             ],
           ),
@@ -105,12 +125,24 @@ class _PowerPlantHomePageState extends State<PowerPlantHomePage> {
             }
 
             return TabBarView(
+              controller: _tabController,
               children: tabs.map((tab) => tab.tabPage(context)).toList(),
             );
           }),
         ),
       ),
     );
+  }
+
+  void sendScreenTabViewEvent(int index) {
+    final currentPath = tabs[index].tabPageRoute;
+    logD('Tab in TabChanged. route: $currentPath');
+
+    // FirebaseAnalyticsObserverのコードを流用
+    final screenName = mindenNameExtractor(RouteSettings(name: currentPath));
+    if (screenName != null) {
+      FirebaseAnalytics.instance.setCurrentScreen(screenName: screenName);
+    }
   }
 
   void navigateByDynamicLinksIfNeeded(Uri uri) {
