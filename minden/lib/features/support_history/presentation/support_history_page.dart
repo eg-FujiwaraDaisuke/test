16f5d173527c1f2c1a524b91860669016c9e1b0d
dasshimo <060417@gmail.com>
2022-04-03 20:45:07 +0900

    事例の部分のアイテムを表示するwidget用意

--- minden/lib/features/support_history/presentation/support_history_page.dart
+++ minden/lib/features/support_history/presentation/support_history_page.dart
@@ -42,4 +42,105 @@ class SupportHistoryPage extends StatelessWidget {
       ),
     );
   }
+
+
+class _CaseWidget extends StatelessWidget {
+  const _CaseWidget({
+    required this.balloonText,
+    required this.imagePath,
+    required this.imageLabelText,
+    required this.mainText,
+  });
+
+  final String balloonText;
+  final String imagePath;
+  final String imageLabelText;
+  final String mainText;
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.stretch,
+      children: [
+        Container(
+          decoration: BoxDecoration(
+            color: const Color(0xFF75C975),
+            borderRadius: BorderRadius.circular(10),
+          ),
+          child: Padding(
+            padding: const EdgeInsets.symmetric(
+              vertical: 8,
+              horizontal: 16,
+            ),
+            child: Center(
+              child: Text(
+                balloonText,
+                style: const TextStyle(
+                  color: Colors.white,
+                  fontSize: 16,
+                  fontWeight: FontWeight.w700,
+                  fontFamily: 'NotoSansJP',
+                  height: 1.4,
+                ),
+              ),
+            ),
+          ),
+        ),
+        const SizedBox(height: 8),
+        ClipRRect(
+          borderRadius: BorderRadius.circular(10),
+          child: AspectRatio(
+            aspectRatio: 1.5,
+            child: Stack(
+              fit: StackFit.expand,
+              children: [
+                Image.asset(
+                  imagePath,
+                  fit: BoxFit.cover,
+                ),
+                Positioned(
+                  right: 0,
+                  bottom: 20,
+                  child: Container(
+                    height: 30,
+                    decoration: const BoxDecoration(
+                      color: Color(0xCCFFFFFF),
+                      borderRadius: BorderRadius.horizontal(
+                        left: Radius.circular(15),
+                      ),
+                    ),
+                    child: Center(
+                      child: Padding(
+                        padding: const EdgeInsets.only(left: 20, right: 16),
+                        child: Text(
+                          imageLabelText,
+                          style: const TextStyle(
+                            color: Color(0xFF32B432),
+                            fontSize: 14,
+                            fontWeight: FontWeight.w600,
+                            fontFamily: 'NotoSansJP',
+                          ),
+                        ),
+                      ),
+                    ),
+                  ),
+                ),
+              ],
+            ),
+          ),
+        ),
+        const SizedBox(height: 8),
+        Text(
+          mainText,
+          style: const TextStyle(
+            color: Color(0xFF7D7E7F),
+            fontSize: 13,
+            fontWeight: FontWeight.w400,
+            fontFamily: 'NotoSansJP',
+            height: 1.6,
+          ),
+        ),
+      ],
+    );
+  }
 }
